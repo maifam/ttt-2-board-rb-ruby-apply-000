576a4ee777abe8aaf947cc23bfd009799bd9d6c8
Mai Pham <maitphm@gmail.com>
2020-10-10 01:21:34 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,2 +1,2 @@
 # Define the variable board below.
-board = ["burgers" "pizza" "sushi" "ramen" "pasta" "seafood" "tacos" "curry" "salad" ""]
+board = ["burgers" "pizza" "sushi" "ramen" "pasta" "seafood" "tacos" "curry" "salad" "ice cream"]
